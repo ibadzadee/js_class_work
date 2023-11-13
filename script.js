3ce44324227f4c8d30d41558e91fe292ac7c12c2
ibadzadeee <sara.ibadzade@gmail.com>
2023-11-13 16:52:22 +0400

    home

---
+++ script.js
@@ -0,0 +1,107 @@
+// ----------------------------------------- 1. -----------------------------------------
+// function myFunction(array){
+//     for(let i =0; i<array.length; i++){
+//         if (i == array[i]){
+//             console.log(array[i])
+//         }
+//     }
+// } 
+
+// const newArray = [1,2,3,1,4]
+// myFunction(newArray)
+
+
+// ----------------------------------------- 2. -----------------------------------------
+
+// const arr_tek = [5, 10, 15, 20, 30]
+// const new_arr = []
+//     for(let i = 0; i < arr_tek.length ; i++){
+//         if(i % 2 !=0 ){
+//             new_arr.push(arr_tek[i])
+//         }   
+//     }
+//     console.log(new_arr);
+
+
+
+// const arr_tek = [5, 10, 15, 20, 30]
+// const new_arr = []
+// function myFunction(array){
+//     for(let i = 0; i < array.length ; i++){
+//         if(i % 2 !=0 ){
+//             new_arr.push(array[i])
+//         }   
+//     }
+//     console.log(new_arr);
+// }
+
+// myFunction(arr_tek)
+
+// ----------------------------------------- 3. -----------------------------------------
+
+// let say = 0
+// function Calc(array , number){
+//     for(let i = 0 ; i < array.length; i++){
+//         if(array[i]<number){
+//             say+=1
+//         }
+//     }
+//     return say
+// }
+
+// const new_arr = [1,20,3, 6]
+
+// console.log(Calc(new_arr, 10))
+
+
+// ----------------------------------------- 4. -----------------------------------------
+
+// new_arr = [1 , 2 , 3 , 4 , 5 , 6]
+
+// function cehck_num(number){
+//     if(number % 2 ==0){
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+
+// }
+
+// console.log(new_arr.filter(cehck_num))
+
+ 
+// ----------------------------------------- 5. -----------------------------------------
+// new_arr = [1 , 2 , 3 , 4 , 5 , 6]
+
+// function square(number) {
+//   return number * number;
+// }
+
+// console.log(new_arr.map(square));
+
+
+// ----------------------------------------- 6. -----------------------------------------
+
+// let a = 0
+// let b = 0
+
+// const Ali  =[4 , 2 , 7]
+// const Davud  =[5 , 6 , 3]
+
+// for(let i = 0; i<Ali.length; i++ ){
+//     if(Ali[i] > Davud[i]){
+//         a++
+//     }
+//     else{
+//         b++
+//     }
+// }
+// console.log(a , b)
+
+
+// ----------------------------------------- Claass_work -----------------------------------------
+
+// const myArray = ["Baku" , "Lankaran" , "Masalli" , "Seki"]
+// let five = myArray.filter((e) => e.length >=5);
+// console.log(five)
